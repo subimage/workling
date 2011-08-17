7bea4bbd69cfcba9f90f19b9b037665ac7772af1
Mike Ragalie <michael.ragalie@verbasoftware.com>
2011-08-17 14:13:28 -0700

    Can specify which queues should be activated for a workling with the QUEUE env variable refs #1587

--- lib/workling/discovery.rb
+++ lib/workling/discovery.rb
@@ -5,10 +5,16 @@ module Workling
   class Discovery
     cattr_accessor :discovered
     @@discovered = []
+    @@queues = ENV['QUEUE'].try(:split, ",")
     
     # requires worklings so that they are added to routing. 
     def self.discover!
-      Dir.glob(Workling.load_path.map { |p| "#{ p }/**/*.rb" }).each { |wling| require wling }
+      Dir.glob(Workling.load_path.map { |p| "#{ p }/**/*.rb" }).each do |wling|
+        name = File.basename(wling, ".rb")
+        if !@@queues || @@queues.include?(name.camelize)
+          require wling
+        end
+      end
     end
   end
 end
